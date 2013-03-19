d4813f5107c86d8f42f6fcdd6ee5274895671d80
Bruce C. Miller <bm3719@gmail.com>
2013-03-19 13:13:56 -0400

    Cleaned up function calls in pexpect scripts.

--- python/pexpect_isca.py
+++ python/pexpect_isca.py
@@ -3,14 +3,14 @@
 import pexpect
 
 child = pexpect.spawn ('telnet bbs.iscabbs.com')
-child.expect ('Name: ')
-child.sendline ('Guest')
+child.expect('Name: ')
+child.sendline('Guest')
 # Wait for the continue prompt, then hit return.
-child.expect ('continue...', timeout=10)
-child.sendline ('')
+child.expect('continue...', timeout=10)
+child.sendline('')
 # I never want to read first few forums, so skip them.
-child.expect ('->')
-child.send ('sgsgsg')
+child.expect('->')
+child.send('sgsgsg')
 # We're logged in, so let's get into interactive mode and start wasting time.
 print("Escape character is '^]'.\n")
 child.interact()
--- python/pexpect_weather.py
+++ python/pexpect_weather.py
@@ -6,14 +6,14 @@ child = pexpect.spawn ('telnet rainmaker.wunderground.com')
 # Dump output to screen.
 child.logfile = sys.stdout
 # First prompt just expects a return.
-child.expect ('continue:')
-child.sendline ('')
+child.expect('continue:')
+child.sendline('')
 # Wait for the code prompt, then enter "RIC".
-child.expect ('code-- ')
-child.sendline ('RIC')
+child.expect('code-- ')
+child.sendline('RIC')
 # Go ahead and exit.
-child.expect ('exit: ')
-child.send ('X')
+child.expect('exit: ')
+child.send('X')
 # Cleanup.
 if child.isalive():
     child.close()
