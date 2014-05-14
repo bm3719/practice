ce619ce7c549b8382c80824f9701b353b56b67e5
Bruce C. Miller <bmiller@whiteboardfederal.com>
2014-05-14 10:29:21 -0400

    Solved #90.

--- clojure/4clojure.clj
+++ clojure/4clojure.clj
@@ -437,12 +437,24 @@
 (= (#(map read-string (map str (seq (str (* %1 %2))))) 999 99) [9 8 9 0 1])
 
 ;; #90: Cartesian Product
-;; (= (__ #{"ace" "king" "queen"} #{"♠" "♥" "♦" "♣"})
-;;    #{["ace"   "♠"] ["ace"   "♥"] ["ace"   "♦"] ["ace"   "♣"]
-;;      ["king"  "♠"] ["king"  "♥"] ["king"  "♦"] ["king"  "♣"]
-;;      ["queen" "♠"] ["queen" "♥"] ["queen" "♦"] ["queen" "♣"]})
-;; (= (__ #{1 2 3} #{4 5})
-;;    #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})
-;; (= 300 (count (__ (into #{} (range 10))
-;;                   (into #{} (range 30)))))
-  
+;; There's probably a cleaner way to do this.
+(= ((fn f [l1 l2] (if (= (count l1) 1) (into #{} (map #(vector (first l1) %) l2))
+                      (into #{} (concat (f (rest l1) l2) (map #(vector (first l1) %) l2)))))
+    #{"ace" "king" "queen"} #{"♠" "♥" "♦" "♣"})
+   #{["ace"   "♠"] ["ace"   "♥"] ["ace"   "♦"] ["ace"   "♣"]
+     ["king"  "♠"] ["king"  "♥"] ["king"  "♦"] ["king"  "♣"]
+     ["queen" "♠"] ["queen" "♥"] ["queen" "♦"] ["queen" "♣"]})
+(= ((fn f [l1 l2] (if (= (count l1) 1) (into #{} (map #(vector (first l1) %) l2))
+                      (into #{} (concat (f (rest l1) l2) (map #(vector (first l1) %) l2)))))
+    #{1 2 3} #{4 5})
+   #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})
+(= 300 (count ((fn f [l1 l2] (if (= (count l1) 1) (into #{} (map #(vector (first l1) %) l2))
+                                 (into #{} (concat (f (rest l1) l2) (map #(vector (first l1) %) l2)))))
+               (into #{} (range 10))
+                  (into #{} (range 30)))))
+
+;; #63: Group a Sequence
+;; (= (__ #(> % 5) [1 3 6 8]) {false [1 3], true [6 8]})
+;; (= (__ #(apply / %) [[1 2] [2 4] [4 6] [3 6]])
+;;    {1/2 [[1 2] [2 4] [3 6]], 2/3 [[4 6]]})
+;; (= (__ count [[1] [3]], 2 [[1 2] [2 3]], 3 [[1 2 3]]})
