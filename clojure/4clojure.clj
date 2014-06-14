fea4b583a448f0be0e058b456ce55658388453c3
Bruce C. Miller <bm3719@gmail.com>
2014-06-14 07:24:34 -0400

    Solved #147.

--- clojure/4clojure.clj
+++ clojure/4clojure.clj
@@ -699,7 +699,44 @@
      7 5/7 2 3/5) 210)
 
 ;; #147: Pascal's Trapezoid
-;; (= (second (__ [2 3 2])) [2 5 5 2])
-;; (= (take 5 (__ [1])) [[1] [1 1] [1 2 1] [1 3 3 1] [1 4 6 4 1]])
-;; (= (take 2 (__ [3 1 2])) [[3 1 2] [3 4 3 2]])
-;; (= (take 100 (__ [2 4 2])) (rest (take 101 (__ [2 2]))))
+(= (second ((fn [col]
+              (iterate
+               (fn [prev-row]
+                 (concat [(first prev-row)]
+                         (map #(apply +' %) (partition 2 1 prev-row))
+                         [(last prev-row)])) col))
+            [2 3 2])) [2 5 5 2])
+(= (take 5 ((fn [col]
+              (iterate
+               (fn [prev-row]
+                 (concat [(first prev-row)]
+                         (map #(apply +' %) (partition 2 1 prev-row))
+                         [(last prev-row)])) col))
+            [1])) [[1] [1 1] [1 2 1] [1 3 3 1] [1 4 6 4 1]])
+(= (take 2 ((fn [col]
+              (iterate
+               (fn [prev-row]
+                 (concat [(first prev-row)]
+                         (map #(apply +' %) (partition 2 1 prev-row))
+                         [(last prev-row)])) col))
+            [3 1 2])) [[3 1 2] [3 4 3 2]])
+(= (take 100 ((fn [col]
+                (iterate
+                 (fn [prev-row]
+                   (concat [(first prev-row)]
+                           (map #(apply +' %) (partition 2 1 prev-row))
+                           [(last prev-row)])) col))
+              [2 4 2]))
+   (rest (take 101 ((fn [col]
+                      (iterate
+                       (fn [prev-row]
+                         (concat [(first prev-row)]
+                                 (map #(apply +' %) (partition 2 1 prev-row))
+                                 [(last prev-row)])) col))
+                    [2 2]))))
+
+;; #173: Intro to Destructuring 2
+;; (= 3
+;;    (let [[__] [+ (range 3)]] (apply __))
+;;    (let [[[__] b] [[+ 1] 2]] (__ b))
+;;    (let [[__] [inc 2]] (__)))
