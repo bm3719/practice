4b6a2214ca01b3936055a132c7376fddc1428d13
Bruce C. Miller <bm3719@gmail.com>
2014-06-27 01:37:42 -0400

    * clojure/4clojure.clj: Solved #76.

--- clojure/4clojure.clj
+++ clojure/4clojure.clj
@@ -1168,10 +1168,17 @@
         [{} #{} [] ()]))
 
 ;; #76: Intro to Trampoline
-;; (= __
-;;    (letfn
-;;        [(foo [x y] #(bar (conj x y) y))
-;;         (bar [x y] (if (> (last x) 10)
-;;                      x
-;;                      #(foo x (+ 2 y))))]
-;;      (trampoline foo [] 1)))
+(= [1 3 5 7 9 11]
+   (letfn
+       [(foo [x y] #(bar (conj x y) y))
+        (bar [x y] (if (> (last x) 10)
+                     x
+                     #(foo x (+ 2 y))))]
+     (trampoline foo [] 1)))
+
+;; #80: Perfect Numbers
+;; (= (__ 6) true)
+;; (= (__ 7) false)
+;; (= (__ 496) true)
+;; (= (__ 500) false)
+;; (= (__ 8128) true)
