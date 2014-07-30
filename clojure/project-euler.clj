78557cd57dea742a3a3f46c16544cf5a73038055
Bruce C. Miller <bm3719@gmail.com>
2014-07-30 13:46:34 -0400

    Solved #16.

--- clojure/project-euler.clj
+++ clojure/project-euler.clj
@@ -596,3 +596,23 @@
 ;;
 ;; What is the sum of the digits of the number 2^1000.
 
+(->> (repeat (bigint 2))
+     (take 1000)
+     (reduce *)
+     (str)
+     (take 1000)
+     (map (comp read-string str))
+     (apply +))
+
+;; #17: Number letter counts
+;;
+;; If the numbers 1 to 5 are written out in words: one, two, three, four, five,
+;; then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+;;
+;; If all the numbers from 1 to 1000 (one thousand) inclusive were written out
+;; in words, how many letters would be used?
+;;
+;; NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and
+;; forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20
+;; letters. The use of "and" when writing out numbers is in compliance with
+;; British usage.
