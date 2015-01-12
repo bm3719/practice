2be6788f18332c4157af17f4ef84e6fadd08f4ec
Bruce C. Miller <bm3719@gmail.com>
2015-01-12 16:16:43 -0500

    Added demonstration of more data validation types and added some field transform functions.

--- clojure/schema-test/dev/user.clj
+++ clojure/schema-test/dev/user.clj
@@ -13,53 +13,82 @@
   "A confirmation function to run from the user REPL" []
   "user namespace active.")
 
-(def data
+;;; Field transform functions.
+
+(defn str->int
+  "Safely converts a string to an int, throwing an exception if not." [s]
+  (if (integer? (read-string s))
+    (read-string s)
+    (throw (Exception. (str "Not an integer: " s)))))
+
+(defn interpret-date-str
+  "Converts a date string of the form YYYY/MM/DD to a clj-time object.  Just a
+  test function.  Will need a better one in production."
+  [dstr]
+  (try
+    (apply t/date-time (map read-string (clojure.string/split dstr #"/")))
+    (catch Exception e (str "Unable to parse date from " dstr))))
+
+;; A simple test schema.
+(def test-schema
   {:a (s/pred (and s/Int pos?))})
 
 ;; The final schema that the data must match.
 (def source1-schema
   {:uic (s/pred (and s/Str #(= (count %) 6)))
    :free-text s/Str
-   ;; :date-something-happened org.joda.time.DateTime
-   ;; :enum-value (s/enum "DIV" "BDE" "BN" "CO")
-   ;; :some-number s/Int
-   ;; :some-pos-number (s/pred (and s/Int pos?))
+   :date-something-happened org.joda.time.DateTime
+   :enum-value (s/enum "DIV" "BDE" "BN" "CO")
+   :some-number s/Int
+   :some-pos-number (s/pred (and s/Int pos?))
    ;; :some-2char-string-that-could-be-blank (s/pred (or s/))
    })
 
-;; The "raw" input data.
+;; An example of raw input data that transforms properly and passes validation.
 (def source1-test-data
   {:uic "WAAXFF"
    :free-text "sldfkjsdljfkds"
-   ;; :date-something-happened "2013/01/01"
-   ;; :enum-value "DIV"
-   ;; :some-number "-2"
-   ;; :some-pos-number "12"
+   :date-something-happened "2013/01/01"
+   :enum-value "DIV"
+   :some-number "-2"
+   :some-pos-number "12"
    ;; :some-2char-string-that-could-be-blank ""
    })
 
+;; An example of raw input data that has both transform and validation errors.
 (def source1-bad-test-data
   {:uic "BLAPFFF"
    :free-text 3
+   :date-something-happened "2011/JUL/01"
+   :enum-value "SDKJVF"
+   :some-number "NAN"
+   :some-pos-number "-2"
+   ;; :some-2char-string-that-could-be-blank "FFFF"
    :some-extra-field "sdf"})
 
 ;; A collection of operations, to be performed on the test data, prior to it
 ;; being validated.
+;;
+;; TODO: Make this a list of function symbols that are applied in sequence.
 (def source1-fns
   {:uic #(->> % (take 6) (apply str))
-   :free-text identity})
+   :free-text identity
+   :date-something-happened interpret-date-str
+   :enum-value identity
+   :some-number str->int
+   :some-pos-number str->int})
 
 (defn find-missing-keys
   "Find if there exist keys in the schema that aren't in the data and return
   them." [schema data]
-  ())
+  (clojure.set/difference (set (keys schema)) (set (keys data))))
 
 (defn find-extra-keys
   "Find if there exist keys in the data that aren't in the schema and return
   them." [schema data]
-  ())
+  (clojure.set/difference (set (keys data)) (set (keys schema))))
 
-(defn encapsulate-validation
+(defn validate-map
   "Encapsulate a validation and return true if successful, otherwise return a
   structure that extracts the failed keys and reasons from the exception
   message.  Will first apply the data-transforms to the data collection, then
@@ -81,5 +110,6 @@
           ;; If the validation fails, grab the message.
           (catch clojure.lang.ExceptionInfo e [k (.getMessage e)])
           ;; Catch all other exceptions.  These are probably data-transform
-          ;; exceptions, like if the transform calls first on an integer.
+          ;; exceptions, like if the transform calls first on an integer, or
+          ;; it's missing completely.
           (catch Exception e [k (str "Transformation error: " (.getMessage e))])))))
