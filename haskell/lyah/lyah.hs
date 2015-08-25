3a5e05287d356aab2f748e542da6c3798eee9e90
Bruce C. Miller <bm3719@gmail.com>
2015-08-25 08:33:10 -0400

    Add progress on chapter 4

--- haskell/lyah/lyah.hs
+++ haskell/lyah/lyah.hs
@@ -151,3 +151,21 @@ bmiTell bmi
   | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
   | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
   | otherwise  = "You're a whale, congratulations!"
+
+max' :: (Ord a) => a -> a -> a
+max' a b | a > b     = a
+         | otherwise = b
+
+compare' :: (Ord a) => a -> a -> Ordering
+compare' a b | a > b     = GT
+             | a < b     = LT
+             | otherwise = EQ
+
+-- BMI calculator using `where'.
+bmiTell' :: (RealFloat a) => a -> a -> String
+bmiTell' weight height
+  | bmi <= 18.5 = "You're underweight, you emo, you!"
+  | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+  | otherwise  = "You're a whale, congratulations!"
+  where bmi = weight / height ^ 2
