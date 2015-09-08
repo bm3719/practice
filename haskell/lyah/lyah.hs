47b04d46bfab5c540b5c65c4fb2272d99d81cc90
Bruce C. Miller <bm3719@gmail.com>
2015-09-08 00:47:43 -0400

    Finish chapter 8

--- haskell/lyah/lyah.hs
+++ haskell/lyah/lyah.hs
@@ -18,6 +18,7 @@ main = do
   let full = first ++ " " ++ last
   putStrLn ("Pleased to meet you, " ++ full ++ "!")
 
+
 --- Chapter 2: Starting Out
 
 doubleMe x = x * x
@@ -90,6 +91,7 @@ removeOdd xxs = [[x | x <- xs, even x] | xs <- xxs]
 -- A list of all right triangles.
 rts = [(a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
 
+
 --- Chapter 3: Types and Typeclasses
 
 -- Types: Int (bounded), Integer (unbounded), Float, Double, Bool, Char.
@@ -109,6 +111,7 @@ rts = [(a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
 
 -- fromIntegral: Promote an Integral type to the more general Num.
 -- fromIntegral (length [1,2,3]) + 3.4
+
 
 --- Chapter 4: Syntax in Functions
 
@@ -246,6 +249,7 @@ fizzBuzz' x | mod x 15 == 0 = "FizzBuzz"
             | otherwise    = show x
 -- map fizzBuzz' [1..100]
 
+
 --- Chapter 5: Recursion
 
 maximum' :: (Ord a) => [a] -> a
@@ -308,6 +312,7 @@ quicksort (x:xs) =
   let smallerSorted = quicksort [a | a <- xs, a <= x]
       biggerSorted = quicksort [a | a <- xs, a > x]
   in  smallerSorted ++ [x] ++ biggerSorted
+
 
 --- Chapter 6: Higher order functions
 
@@ -466,6 +471,7 @@ pF = ceiling . negate . tan . cos . max 50
 -- sumAnswer using (.)
 sumAnswer'' :: Int
 sumAnswer'' = (+1) . length . takeWhile (<1000) . scanl1 (+) . map sqrt $ [1..]
+
 
 -- Chapter 7: Modules
 
@@ -752,4 +758,421 @@ textIntersection = Set.intersection (Set.fromList text1) (Set.fromList text2)
 -- folder called that with other modules within it.  Then name them stuff like
 -- Geometry.Sphere.  Not doing that here since it's pretty obvious.
 
+
 --- Chapter 8: Making Our Own Types and Typeclasses
+
+--- Algebraic data types: Define with the `data' keyword and its value
+--- constructors, separated by `|' (or).  Value constructors are actually
+--- functions.
+
+-- Make a data type for a shape.  A circle is represented by the coordinates of
+-- its center and its radius.  A rectangle is represented by the coordinates of
+-- its upper left and lower right corner.
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+           deriving (Show)
+
+-- Make a function that takes a Shape and returns its surface.
+surface :: Shape -> Float
+surface (Circle _ _ r) = pi * (r ^ 2)
+surface (Rectangle x1 y1 x2 y2) = abs ((x1 - x2) * (y1 - y2))
+
+-- show functions: The string representation of a value.  In order to make the
+-- Shape type part of the Show typeclass, using `deriving (Show)'.
+
+-- Concentric circles with different radii.
+-- map (Circle 10 20) [4, 5, 6, 7]
+
+-- Create a better shape that uses an intermediate type.  Using the same name
+-- as the type is idiomatic if there's only one value constructor.
+data Point = Point Float Float deriving (Show)
+data Shape' = Circle' Point Float | Rectangle' Point Point
+            deriving (Show)
+
+-- Create new surface function for the new shape type.
+surface' :: Shape' -> Float
+surface' (Circle' _ r) = pi * (r ^ 2)
+surface' (Rectangle' (Point x1 y1) (Point x2 y2)) =
+  abs $ (x1 - x2) * (y1 - y2)
+
+-- Create function that moves a shape on the x and y axis.
+nudge :: Shape' -> Float -> Float -> Shape'
+nudge (Circle' (Point x y) r) dx dy = Circle' (Point (x + dx) (y + dy)) r
+nudge (Rectangle' (Point x1 y1) (Point x2 y2)) dx dy =
+  Rectangle' (Point (x1 + dx) (y1 + dy)) (Point (x2 + dx) (y2 + dy))
+
+-- Exporting types: In the module block, use the syntax `(..)' to export all
+-- value constructors for a type.
+-- module Shapes
+--        ( Point(..)
+--        , Shape(..)
+--        , ...
+
+-- One could also omit the `(..)' to force the use of other functions to call
+-- value constructors.  Data.Map does this.
+
+--- Record syntax
+
+-- Make a record for a person containing: first name, last name, age, height,
+-- phone number, and favorite ice cream flavor.
+-- data Person = Person String String Int Float String String
+--             deriving (Show)
+
+-- Make one better than the above using record syntax.  Automatically creates
+-- lookup functions for each field, with their field name being the function
+-- name.
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String
+                     } deriving (Show)
+
+-- Instantiating a type allows for either the normal syntax (parameters in order), or record syntax where they are in any order, specified by name.
+data Car = Car {company :: String, model :: String, year :: Int}
+         deriving (Show)
+
+aCar = Car {company="Ford", year=1967, model="Mustang"}
+
+--- Type parameters: Type constructors can take types as parameters and produce
+--- new types, unlike data constructors which just produce new values of a
+--- type.
+
+-- Maybe is a type constructor (a is a type parameter):
+-- data Maybe a = Nothing | Just a
+
+-- Maybe by itself is not a type, whereas `Maybe Num' is.
+
+-- Don't use typeclass constraints in data declarations, as it will
+-- unnecessarily constrain functions against it.
+
+-- Implement a 3D vector type and some operations.
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+vplus (Vector i j k) (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+-- Multiply Vector with a scalar.
+vectMult :: (Num t) => Vector t -> t -> Vector t
+vectMult (Vector i j k) m = Vector (i*m) (j*m) (k*m)
+
+-- Multiply two Vectors, returning a scalar.
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+scalarMult (Vector i j k) (Vector l m n) = i*l + j*m + k*n
+
+-- Type constructors vs. value constructor: Type constructors are before the =,
+-- value constructors after.
+
+--- Derived instances: Automatically making our type an instance of a
+--- typeclass.
+
+-- Custom person data type.
+data Person' = Person' { pFirstName :: String
+                       , pLastName :: String
+                       , pAge :: Int
+                       } deriving (Eq, Show, Read)
+
+-- Eq: Since all fields are of typeclass Eq, this can derive from Eq.  ==,
+-- elem, and other functions that have a class constraint of `Eq a' will then
+-- work.
+
+-- Show, Read: Show allows the instance to be rendered as a string. Read does
+-- the opposite.
+
+-- read "Person' {pFirstName=\"Bric\", pLastName=\"Mahr\", pAge=33}" :: Person'
+
+-- Make an Enum and Bounded algebraic data type.  Since all value constructors
+-- are nullary, making it Enum is possible.  Implementing the Bounded typeclass
+-- means it has a lowest and highest possible value.
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+         deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+-- Being derived from Bounded, minBound and MaxBound work.
+-- minBound :: Day
+
+-- Enum gives us the succ and pred functions, and allows for ranges.
+-- succ Monday
+-- [Monday .. Sunday]
+-- [minBound .. maxBound] :: [Day]
+
+--- Type synonyms: Give types different names.
+
+-- String is an example:
+-- type String = [Char]
+
+-- This is the only use of the `type' keyword in the language.
+
+phoneBook' :: [(String, String)]
+phoneBook' =
+  [ ("Fred", "555-1200")
+  , ("Gary", "555-1212")
+  ]
+
+type Name = String
+type PhoneNumber = String
+type PhoneBook = [(Name, PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
+
+-- inPhoneBook "Gary" "555-1212" phoneBook'
+
+-- Like with data types, type synonyms can be parameterized.
+type AssocList k v = [(k, v)]
+
+-- Type synonyms can be partially applied.  Here's one that makes a Map of Ints
+-- to something.
+type IntMap v = Map.Map Int v
+-- This would also work:
+-- type IntMap = Map.Map Int
+
+-- Either: Has two value constructors.  If Left is used, its contents are type
+-- a, if Right is used, its contents are type b.  It's defined something like
+-- this:
+-- data Either a b = Left a | Right b deriving (Eq, Ord, Read, Show)
+
+-- Either can be useful in the same way Maybe is, except that Either can convey
+-- more information than Nothing (which is just a signal that something
+-- failed).  Typically errors, use the Left value constructor and results use
+-- the Right.
+
+-- Example: Assign locker codes when a locker number is requested.  If the
+-- locker is already used, don't tell the code.
+data LockerState = Taken | Free deriving (Show, Eq)
+type Code = String
+type LockerMap = Map.Map Int (LockerState, Code)
+
+-- Failure can happen in one of two ways: The locker is taken or the locker
+-- doesn't exist.  Create a lookup function using Either to represent this.
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+  case Map.lookup lockerNumber map of
+    Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist."
+    Just (state, code) -> if state /= Taken
+                         then Right code
+                         else Left $ "Locker " ++ show lockerNumber ++ " is already taken."
+
+lockers :: LockerMap
+lockers = Map.fromList
+    [ (100,(Taken,"ZD39I"))
+    , (101,(Free,"JAH3I"))
+    , (103,(Free,"IQSA9"))
+    , (105,(Free,"QOTSA"))
+    , (109,(Taken,"893JJ"))
+    , (110,(Taken,"99292"))
+    ]
+
+-- Recursive data structures
+
+-- List is a recursive data type.  Here's a custom one.
+-- data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+-- Cons 4 (Cons 5 (Cons 6 Empty))
+
+-- Defining functions as infix: Fixity declarations can be used to define
+-- left/right-associativity and how tightly the operator binds.  For example,
+-- (*) is `infixl 7 *', (+) is `infixl 6'.
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+-- 1 :-: 4 :-: Empty
+
+-- Make a function analogous to (++).
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty      .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
+-- (1 :-: 4 :-: Empty) .++ (2 :-: Empty)
+
+-- Implement a binary search tree.
+data Tree a = EmptyTree | Node a (Tree a) (Tree a)
+            deriving (Show, Read, Eq)
+
+-- Make singleton and insert functions.
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+insertTree :: (Ord a) => a -> Tree a -> Tree a
+insertTree x EmptyTree = singleton x
+insertTree x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (insertTree x left) right
+  | x > a = Node a left (insertTree x right)
+-- foldr insertTree EmptyTree [9,4,3,2,3,5,1,3]
+
+-- Implement elem for Trees.
+elemTree :: (Ord a) => a -> Tree a -> Bool
+elemTree x EmptyTree = False
+elemTree x (Node a left right)
+  | x == a = True
+  | x < a = elemTree x left
+  | x > a = elemTree x right
+-- elemTree 5 $ foldr insertTree EmptyTree [9,4,3,2,3,5,1,3]
+
+--- Typeclasses 102: Making typeclasses.
+
+-- Example: The Eq typeclass is defined as:
+-- class Eq a where
+--   (==) :: a -> a -> Bool
+--   (/=) :: a -> a -> Bool
+--   x == y = not (x /= y)
+--   x /= y = not (x == y)
+
+-- Make a new type and make it an instance of Eq.
+data TrafficLight = Red | Yellow | Green
+
+-- Since == and /= are defined as mutually recursive, we only have to overwrite
+-- one in the instance declaration.  If neither were defined in the typeclass,
+-- we would have to implement both.
+instance Eq TrafficLight where
+  Red == Red = True
+  Green == Green = True
+  Yellow == Yellow = True
+  _ == _ = False
+
+-- Implement Show by hand.  By default, directly deriving Show will convert the
+-- value constructors to strings.  This one adds the word "light".
+instance Show TrafficLight where
+  show Red = "Red light"
+  show Green = "Green light"
+  show Yellow = "Yellow light"
+
+-- Subclasses: A class constraint on a class declaration.  Here, a has to be an
+-- instance of Eq before it can be an instance of Num.
+-- class (Eq a) => Num a where ...
+
+-- For non-concrete types (types that are defined as type constructors), like
+-- Maybe, instead of writing out their instance declarations for all
+-- combinations (e.g. Maybe Int, Maybe String, etc), do it like this:
+-- instance (Eq m) => Eq (Maybe m) where
+--   Just x == Just y = x == y
+--   Nothing == Nothing = True
+--   _ == _ = False
+
+-- Note: To check the instances of a typeclass, use `:info <class>' in the
+-- REPL.  This also works for type constructors.  Super useful.
+
+-- Implement a YesNo system like JavaScript's booleans using the class system.
+class YesNo a where
+  yesno :: a -> Bool
+
+-- In JavaScript, 0 is false and everything else true.
+instance YesNo Int where
+  yesno 0 = False
+  yesno _ = True
+
+-- In JavaScript, empty lists/strings are false and everything else true.
+instance YesNo [a] where
+  yesno [] = False
+  yesno _  = True
+
+-- Booleans are the same.
+instance YesNo Bool where
+  yesno = id
+
+-- Maybe instance
+instance YesNo (Maybe m) where
+  yesno (Just _) = True
+  yesno Nothing  = False
+
+-- Maybe an empty Tree (defined earlier) false and others true.
+instance YesNo (Tree t) where
+  yesno EmptyTree = False
+  yesno _         = True
+
+-- For TrafficeLights, make just Red false.
+instance YesNo TrafficLight where
+  yesno Red = False
+  yesno _   = True
+
+-- Make a function that mimics a JavaScript if-else statement with the first
+-- parameter being the predicate.
+yesNoIf :: (YesNo y) => y -> a -> a -> a
+yesNoIf y a b = if yesno y then a else b
+
+--- The Functor typeclass: Represents things that can be mapped over (or a
+--- homomorphism between categories).
+
+-- Functor's typeclass defines fmap but no implementation.
+-- class Functor f where
+--   fmap :: (a -> b) -> f a -> f b
+
+-- Here, f is not a concrete type, but a type constructor that takes one type
+-- parameter.
+
+-- List is actually an instance of the Functor typeclass.  [] is a type
+-- constructor that can produce types like [Char] and [Int].
+-- instance Functor [] where
+--   fmap = map
+
+-- Types that act like a box can be functors.  Maybe is also a box and is a
+-- functor.
+
+-- instance Functor Maybe where
+--   fmap f (Just x) = Just (f x)
+--   fmap f Nothing  = Nothing
+
+-- fmap (+1) (Just 1)
+
+instance Functor Tree where
+  fmap _ EmptyTree = EmptyTree
+  fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
+-- fmap (+1) $ foldr insertTree EmptyTree [9,2,4,6,1,3,2,4]
+
+-- Type constructors with multiple type parameters.  Either takes two, but can
+-- still be made a functor.
+
+-- This uses currying on the type constructor, which for Either, makes sense
+-- considering the Left value constructor can be of a different type than
+-- Right.  Also the way Either is used is such that Left is used for errors.
+-- instance Functor (Either a) where
+--   fmap f (Right x) = Right (f x)
+--   fmap f (Left x)  = Left x
+
+-- Figure out how Map k is made a functor.  I think this is right.
+-- instance (Ord k) => Functor (Map.Map k) where
+--    fmap f m = Map.fromList $ map (\(k, v) -> (k, f v)) $ Map.toList m
+
+-- fmap (+1) $ Map.fromList [("a",1), ("b",2)]
+
+--- Kinds and some type-foo: (Not sure I completely understand this stuff, so
+--- give it another read or read it more carefully in another book).
+
+-- In kind notation, a * means that the type is a concrete type.  * -> * means
+-- that a type constructor takes one concrete type and returns another concrete
+-- type.  Kind checking can occur on partially applied type constructors, like
+-- with `:k Either String'.
+
+-- Explaining Functor's kind: Functor has kind `Functor :: (* -> *) ->
+-- Constraint'.  This means that it is a typeclass that requires types that
+-- are to be made instances of it to be of kind `* -> *'.
+
+-- How to make a type that is an instance of this?
+class Tofu t where
+  tofu :: j a -> t a j
+
+-- Tofu has a kind of `Tofu :: (* -> (* -> *) -> *) -> Constraint'.  `j a' has
+-- to have a kind of *.  If we assume * for a, then j has to have a kind of `*
+-- -> *'.  t has to produce a concrete value, so we know that t has a kind of
+-- `* -> (* -> *) -> *'.
+
+-- Let's make a type with a kind of `* -> (* -> *) -> *'.
+data Frank a b = Frank { frankField :: b a }
+               deriving (Show)
+
+-- λ> :t Frank {frankField = Nothing}
+-- Frank {frankField = Nothing} :: Frank a Maybe
+
+-- Make Frank an instance of Tofu.
+instance Tofu Frank where
+  tofu x = Frank x
+
+-- λ> tofu (Just 'a') :: Frank Char Maybe
+-- Frank {frankField = Just 'a'}
+
+-- Make a type to make an instance of Functor.
+data Barry t k p = Barry { yabba :: p, dabba :: t k }
+
+instance Functor (Barry a b) where
+  fmap f (Barry {yabba = x, dabba = y}) = Barry {yabba = f x, dabba = y}
+
+
+--- Chapter 9: Input and Output
