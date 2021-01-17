2cf4f06f76ec4110ea062e080150fbb91f344f54
Bruce C. Miller <bm3719@gmail.com>
2021-01-17 17:33:40 -0500

    Add lab 7

---
+++ python/pi_lab/7_adc.py
@@ -0,0 +1,35 @@
+# Use ADC microcontroller with poteniometer.
+# NOTE: Requires I2C enabled in OS, smbus package, and custom ADCDevice
+# package.
+import time
+from ADCDevice import *
+
+adc = ADCDevice()
+
+def setup():
+    global adc
+    if (adc.detectI2C(0x48)): # If pcf8591.
+        adc = PCF8591()
+    elif (adc.detectI2C(0x4b)): # If ads7830.
+        adc = ADS7830()
+    else:
+        print("No correct I2C address found, \n"
+               "Use command 'i2cdetect -y 1' to check I2C address")
+        exit(-1)
+
+def loop():
+    while True:
+        value = adc.analogRead(0)  # Read channel 0.
+        voltage = value / 255.0 * 3.3
+        print ("ADC value: %d, Voltage: %,2f" %(value, voltage))
+        time.sleep(0.1)
+
+def destroy():
+    adc.close()
+
+print('Program is starting... \n')
+try:
+    setup()
+    loop()
+except KeyboardInterrupt:
+    destroy()
