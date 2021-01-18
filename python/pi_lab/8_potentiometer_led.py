cac6370e84965e8a029139f160eb7afd4886d545
Bruce C. Miller <bm3719@gmail.com>
2021-01-17 21:19:54 -0500

    Add lab 8

--- python/pi_lab/8_potentiometer_led.py
+++ python/pi_lab/8_potentiometer_led.py
@@ -1,9 +1,9 @@
-# Use ADC microcontroller with potentiometer.
-# NOTE: Requires I2C enabled in OS, smbus package, and custom ADCDevice
-# package.
+# Use ADC microcontroller with potentiometer to control LED brightness.
+import RPi.GPIO as GPIO
 import time
 from ADCDevice import *
 
+ledPin = 11
 adc = ADCDevice()
 
 def setup():
@@ -16,16 +16,24 @@ def setup():
         print("No correct I2C address found, \n"
                "Use command 'i2cdetect -y 1' to check I2C address")
         exit(-1)
+    global p
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ledPin, GPIO.OUT)
+    p = GPIO.PWM(ledPin, 1000)
+    p.start(0)
 
 def loop():
     while True:
         value = adc.analogRead(0)  # Read channel 0.
+        p.ChangeDutyCycle(value * 100 / 255) # Map value to duty cycle.
         voltage = value / 255.0 * 3.3
         print ("ADC value: " + str(value) + ", Voltage: " + str(voltage))
         time.sleep(0.1)
 
 def destroy():
     adc.close()
+    GPIO.cleanup() # Release all GPIO.
+
 
 print('Program is starting... \n')
 try:
