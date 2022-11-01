7deb09633a1c49f9f786618f700e49eb730a59e3
Bruce C. Miller <bm3719@gmail.com>
2022-11-01 13:58:48 -0400

    Fix pathing from copying from other template

--- clojurescript/basic-rf/project.clj
+++ clojurescript/basic-rf/project.clj
@@ -30,7 +30,7 @@
                 ;; The presence of a :figwheel configuration here
                 ;; will cause figwheel to inject the figwheel client
                 ;; into your build
-                :figwheel {:on-jsload "basic-app.core/mount-root"
+                :figwheel {:on-jsload "basic-rf.core/mount-root"
                            ;; :open-urls will pop open your application
                            ;; in the default browser once Figwheel has
                            ;; started and compiled your application.
